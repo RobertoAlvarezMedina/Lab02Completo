75b2590988e54da41ee4f80a9594b057b0b0082b
RobertoAlvarezMedina <robert10alvarz@gmail.com>
2018-09-21 15:49:01 -0600

    Completo semana2 Lab02

---
+++ Lab02.Logic/Lab02.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using Lab02.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Person");
+
+            ILogger logger = new Person();
+            Person person = new Person();
+
+            person.Id = 1;
+            person.Name = "Juan";
+            person.LastName = "Perez";
+
+
+
+            string personString = string.Format("Id:{0},Name:{1},LastName:{2}", person.Id, person.Name, person.LastName);
+
+            System.Console.WriteLine(personString);
+            logger.Log("Hello world");
+            System.Console.ReadKey();
+        }
+    }
+}
